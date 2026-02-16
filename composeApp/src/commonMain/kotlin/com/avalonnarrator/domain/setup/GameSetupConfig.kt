c7aff3a54af064a9d97264087b05af7b50b34712
Chris Wong <wongislandd@gmail.com>
2026-02-16 17:35:00 -0500

    add logo to web favicon, update default game setup

--- composeApp/src/commonMain/kotlin/com/avalonnarrator/domain/setup/GameSetupConfig.kt
+++ composeApp/src/commonMain/kotlin/com/avalonnarrator/domain/setup/GameSetupConfig.kt
@@ -5,10 +5,10 @@ import com.avalonnarrator.domain.audio.VoicePackIds
 import com.avalonnarrator.domain.roles.RoleId
 
 data class GameSetupConfig(
-    val playerCount: Int = 5,
+    val playerCount: Int = 7,
     val selectedRoles: Set<RoleId> = setOf(RoleId.MERLIN, RoleId.ASSASSIN),
-    val loyalServantAdjustment: Int = 2,
-    val minionAdjustment: Int = 1,
+    val loyalServantAdjustment: Int = 3,
+    val minionAdjustment: Int = 2,
     val validatorsEnabled: Boolean = true,
     val enabledModules: Set<GameModule> = emptySet(),
     val regularPauseMs: Int = 1_000,
